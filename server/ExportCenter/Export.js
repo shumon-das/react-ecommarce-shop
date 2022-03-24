b280a3950e2d3dc0f37a9cdb5c22442ad3dad6fc
monoranjan.das <monoranjan.das@covianalytics.com>
2022-03-25 01:34:35 +0600

    Completed Excel file creation for backend

--- server/ExportCenter/Export.js
+++ server/ExportCenter/Export.js
@@ -1,5 +1,4 @@
 import ExcelJs from 'exceljs'
-import { response } from 'express'
 import data from '../data/data.js'
 
 function Export(app){
@@ -8,37 +7,87 @@ function Export(app){
             const workBook = new ExcelJs.Workbook()
             const sheet = workBook.addWorksheet('users data')
             
+            // set column 
             sheet.columns = [
-                {header:'ID', key:'id', width:10},
-                {header:'First Name', key:'firstName', width:15},
-                {header:'Last Name', key:'lastName', width:15},
-                {header:'Username', key:'userName', width:25},
-                {header:'Gender', key:'gender', width:10},
-                {header:'E-mail', key:'email', width:25},
-                {header:'Address', key:'address', width:25},
-                {header:'Choice', key:'choice', width:10},
-                {header:'Learn', key:'learn', width:10},
-                {header:'Teach', key:'teach', width:10}
+                {key:'id', width:10},
+                {key:'firstName', width:15},
+                {key:'lastName', width:15},
+                {key:'userName', width:25},
+                {key:'gender', width:10},
+                {key:'email', width:25},
+                {key:'address', width:25},
+                {key:'choice', width:10},
+                {key:'learn', width:10},
+                {key:'teach', width:10},
+                {key:'date', width:15}
             ]
-            
-            sheet.getCell('A1').value = 'My Excel Sheet'
+
+            // add empty row before title
+            sheet.addRow();
+
+            // add title 
+            sheet.addRow({ firstName: 'Users List'});
+
+            // merge two column
+            sheet.mergeCells('B2:C2');
+
+            // set height for title column
+            sheet.getRow(2).height = 30
+
+            // add some style for title 
+            sheet.getRow(2).getCell(2).fill = {type: 'pattern', pattern: 'solid',fgColor: { argb:'5F9EA0' }}
+
+            // add empty row after title and before header column
+            sheet.addRow();
+        
+            // add header column for data
+            sheet.addRow({
+                id: 'ID', 
+                firstName: 'First Name', 
+                lastName: 'Last Name', 
+                userName: 'Username', 
+                gender: 'Gender', 
+                email: 'E-mail', 
+                address: 'Address',
+                choice: 'Choice',
+                learn: 'Learn',
+                teach: 'Teach',
+                date: 'Date'
+            });
+
+            // set height for db data header column
+            sheet.getRow(4).height = 40;
+
+            // add database data (all row)
             data.forEach(user => {
-                sheet.addRow(user)
+                // sheet.addRow(user)
+                sheet.addRow({
+                    id: user.id,
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    userName: user.userName,
+                    gender: user.gender,
+                    email: user.email,
+                    address: user.address,
+                    choice: user.choice,
+                    learn: user.learn,
+                    teach: user.teach,
+                    date: new Date(1970,1,1)
+                });
             })
 
-            sheet.getRow(1).height = 40;
-
-            // css style for selected column   and only for row 1
-            sheet.getRow(1).getCell(1).fill = {type: 'pattern', pattern: 'solid',fgColor: { argb:'5F9EA0' }}
-            sheet.getRow(1).getCell(2).fill = {type: 'pattern', pattern: 'solid',fgColor: { argb:'5F9EA0' }}
-            sheet.getRow(1).getCell(3).fill = {type: 'pattern', pattern: 'solid',fgColor: { argb:'5F9EA0' }}
-            sheet.getRow(1).getCell(4).fill = {type: 'pattern', pattern: 'solid',fgColor: { argb:'5F9EA0' }}
-            sheet.getRow(1).getCell(5).fill = {type: 'pattern', pattern: 'solid',fgColor: { argb:'5F9EA0' }}
-            sheet.getRow(1).getCell(6).fill = {type: 'pattern', pattern: 'solid',fgColor: { argb:'5F9EA0' }}
-            sheet.getRow(1).getCell(7).fill = {type: 'pattern', pattern: 'solid',fgColor: { argb:'5F9EA0' }}
-            sheet.getRow(1).getCell(8).fill = {type: 'pattern', pattern: 'solid',fgColor: { argb:'5F9EA0' }}
-            sheet.getRow(1).getCell(9).fill = {type: 'pattern', pattern: 'solid',fgColor: { argb:'5F9EA0' }}
-            sheet.getRow(1).getCell(10).fill = {type: 'pattern', pattern: 'solid',fgColor: { argb:'5F9EA0'}}
+            /* css style for selected column   and only for row 1 */
+            sheet.getRow(4).getCell(1).fill = {type: 'pattern', pattern: 'solid',fgColor: { argb:'5F9EA0' }}
+            sheet.getRow(4).getCell(2).fill = {type: 'pattern', pattern: 'solid',fgColor: { argb:'5F9EA0' }}
+            sheet.getRow(4).getCell(3).fill = {type: 'pattern', pattern: 'solid',fgColor: { argb:'5F9EA0' }}
+            sheet.getRow(4).getCell(4).fill = {type: 'pattern', pattern: 'solid',fgColor: { argb:'5F9EA0' }}
+            sheet.getRow(4).getCell(5).fill = {type: 'pattern', pattern: 'solid',fgColor: { argb:'5F9EA0' }}
+            sheet.getRow(4).getCell(6).fill = {type: 'pattern', pattern: 'solid',fgColor: { argb:'5F9EA0' }}
+            sheet.getRow(4).getCell(7).fill = {type: 'pattern', pattern: 'solid',fgColor: { argb:'5F9EA0' }}
+            sheet.getRow(4).getCell(8).fill = {type: 'pattern', pattern: 'solid',fgColor: { argb:'5F9EA0' }}
+            sheet.getRow(4).getCell(9).fill = {type: 'pattern', pattern: 'solid',fgColor: { argb:'5F9EA0' }}
+            sheet.getRow(4).getCell(10).fill = {type: 'pattern', pattern: 'solid',fgColor: { argb:'5F9EA0'}}
+            sheet.getRow(4).getCell(11).fill = {type: 'pattern', pattern: 'solid',fgColor: { argb:'5F9EA0'}}
 
             // css style      for each row      and       for each column
             sheet.eachRow({ includeEmpty: true }, function(row, rowNumber){
@@ -46,54 +95,44 @@ function Export(app){
                  cell.font = {
                    name: 'Arial',
                    family: 2,
-                //    bold: false,
+                   bold: false,
                 //    color: {argb: "004e47cc"},
                    size: 10,
                  };
                  cell.alignment = {
                    vertical: 'middle', horizontal: 'center'
                  };
-                //  if (rowNumber <= 10) {
-                //    row.height = 20;
-                //    cell.font = {
-                //      bold: true,
-                //    };
-                //   }
-                //   if (rowNumber >= 10) {
-                //    for (var i = 1; i < 14; i++) {
-                //      if (rowNumber == 10) {
-                //        row.getCell(i).fill = {
-                //          type: 'pattern',
-                //          pattern:'solid',
-                //          fgColor:{argb:'C7C7C7'}
-                //        };
-                //      }
-                //      row.getCell(i).border = {
-                //      top: {style:'thin'},
-                //      left: {style:'thin'},
-                //      bottom: {style:'thin'},
-                //      right: {style:'thin'}
-                //    };
-                //  }
-                // }
                });
-              });
+            });
 
-              // css style for each column   and only for row 1
-              sheet.getRow(1).eachCell((cell, cellNumber) => {
+            // css style for each column   and only for row 1
+            sheet.getRow(4).eachCell((cell, cellNumber) => {
                 cell.font = {
                     color: {argb: "ffffff"},
                     bold: true
+                },
+                cell.border = {
+                    top: {style:'thin'},
+                    left: {style:'thin'},
+                    bottom: {style:'thin'},
+                    right: {style:'thin'}
                 }
-              })
+            })
 
+            // apply css only for a specific cell
+            sheet.getCell('B2').font = { name: 'Comic Sans MS',family: 4,size: 25, underline: true,bold: true, color: {argb: "ebeef2"} };
+
+            // make dynamic file name
             let filename = `users${Math.floor(Math.random() * 50)}.xlsx`
+
+            // write file
             workBook.xlsx.writeFile(filename).then((response) =>{
                 res.send('Excel Created')
             })
+
         }catch(error){
             console.log(error)
-            res.send(error)
+            res.status(500).send(error)
         }   
    
     })
